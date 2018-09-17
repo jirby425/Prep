b3d82c3fd29d32f84049e1f010be08c6615b7925
Jackson <jacksonirby@gmail.com>
2018-09-16 20:07:46 -0500

    added std_obs for python and P3

--- std_objs/std_objs.java
+++ std_objs/std_objs.java
@@ -1,10 +1,10 @@
 
 public class std_objs {
-	
-	public static void main (String[] args)
 
+	public static void main (String[] args){
+
+		
+	}
 
 
 }
-
-
---
+++ std_objs/std_objs.py
@@ -0,0 +1,23 @@
+
+
+
+def listExample():
+    l = []
+    for i in range (0,10):
+        l.append(i)
+    for i in l:
+        print l[i],
+
+def dictExample():
+    d = {"this":1, "is": 2, "a":3, "test":4}
+    print d
+    print d["this"]
+
+    for key in d.keys():
+        print key + ": " + str(d[key])
+
+
+if __name__ == "__main__":
+    print "Hello world"
+    listExample()
+    dictExample()
