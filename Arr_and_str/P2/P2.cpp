62c81618e034cfe4f3b8de9ba7f2fb12889ddb86
Jackson <jacksonirby425@gmail.com>
2018-09-16 19:10:12 -0500

    updating P2.cpp

--- Arr_and_str/P2/P2.cpp
+++ Arr_and_str/P2/P2.cpp
@@ -5,22 +5,30 @@
 using namespace std;
 
 bool checkPerm(string& str1, string& str2){
-	
-		
+
+
 	cout << str1 << " " << str2 <<endl;
-	
+
 	if (str1.length() != str2.length()) return false;
-	
-	int char_count[128];		
-	for(int i = 0; i < str1.length(); i++)
-		
-	
+
+	int char_count[128];
+	for(int i = 0; i < str1.length(); i++){
+		int val = str1[i];
+		char_count[val]++;
+	}
+
+	for(int i = 0; i < str2.length(); i++){
+		int val = str2[i];
+		char_count[val]--;
+		if (char_count[val] < 0)
+			return false;
+	}
 	return true;
 }
 
 
 int main(){
-	
+
 	vector<string> vec1 = {"jackson", "sonjack"};
 	vector<string> vec2 = {"jackson", "notperm"};
 
